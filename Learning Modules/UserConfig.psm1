9f8b5207e37070afc2aa74d3c51f62f319cc0412
Bill Gibson <bill.gibson@microsoft.com>
2018-02-01 14:32:13 -0800

    Update userconfig.psm1 to remove resource group

--- Learning Modules/UserConfig.psm1
+++ Learning Modules/UserConfig.psm1
@@ -1,27 +1,26 @@
 ﻿<#
 .SYNOPSIS
-    Returns the User name and resource group name used during the Wingtip SaaS application deployment.  
-    The values defined here are referenced by the learning module scripts.
+    Returns the User name used during the Wingtip Tickets SaaS application deployment.  
+    The value defined here is used in the learning module scripts.
 #>
 
 
-## Update the two variables below to the values used when the Wingtip SaaS app was deployed.   
+## Update the $User variable below    
 
-$ResourceGroupName = "wingtip-sa-bgx" # the resource group used when the Wingtip SaaS application was deployed. CASE SENSITIVE
-$User              = "bgx"         # the User value entered when the Wingtip SaaS application was deployed
+$User = "<user>"   # the User value entered when the Contoso Concert Hall application was deployed
+
 
 ##  DO NOT CHANGE VALUES BELOW HERE -------------------------------------------------------------
 
 function Get-UserConfig {
 
     $userConfig = @{`
-        ResourceGroupName = $ResourceGroupName     
-        Name =              $User.ToLower()   
+        Name = $User.ToLower()   
     }
    
-    if ($userConfig.ResourceGroupName -eq "<resourcegroup>" -or $userConfig.Name -eq "<user>")
+    if ($userConfig.Name -eq "<user>")
     {
-        throw 'UserConfig is not set.  Modify $ResourceGroupName and $User in UserConfig.psm1 and try again.'
+        throw 'UserConfig is not set.  Modify $User in UserConfig.psm1 and try again.'
     }
 
     return $userConfig
